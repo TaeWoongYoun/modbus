854057e6b36e0c5fb8a765eae9786e676f43daab
xodnd0507 <24457545yong@gmail.com>
2024-06-11 14:04:35 +0900

    test : Fuction button

--- UI.py
+++ UI.py
@@ -1,6 +1,12 @@
 from tkinter import *
 
 tk = Tk()
-label = Label(tk, text='Hello python!')
-label.pack()
+# label = Label(tk, text='Hello python!')
+# label.pack()
+# tk.mainloop()
+
+def event():
+    button['text'] = '버튼 누름!'
+button = Button(tk, text='누르면 함수 실행', command=event)
+button.pack(side=LEFT,padx=10,pady=10) #side로 배치설정, padx로 좌우 여백설정, pady로 상하 여백설정 
 tk.mainloop()
