dd5849424d99bc6ad1c3b1bd6bcd5ecccab8ee3a
xodnd0507 <24457545yong@gmail.com>
2024-07-08 17:10:18 +0900

    feat : Update code

--- read.py
+++ read.py
@@ -7,19 +7,12 @@ instrument.serial.timeout = 1
 instrument.serial.stopbits = 1
 instrument.serial.bytesize = 8
 
-response_count = 0
-max_responses = 1
-while response_count < max_responses:
+for i in range(0, 3):
     try:
         # 모드버스 RTU 프레임 전송 및 데이터 읽기
         response = instrument.read_registers(0, 3)
-        if len(response) >= 3:  # 최소한 거리, 습도, 온도의 응답이 있는 경우
-            humidity = response[0]
-            temperature = response[1]
-            distance = response[2]
-            print(f"거리: {humidity}")
-            print(f"습도: {temperature}")
-            print(f"온도: {distance}")
-            response_count += 1  # 응답 횟수 증가
+        print(f"거리: {response[0]}")
+        print(f"습도: {response[1]}")
+        print(f"온도: {response[2]}")
     except:
         continue  # 예외 발생 시 다시 시도
