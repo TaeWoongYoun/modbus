946374e78179932238409d02be1fbcc899359221
xodnd0507 <24457545yong@gmail.com>
2024-07-09 15:50:55 +0900

    feat : Code refactoring

--- LED.py
+++ LED.py
@@ -1,33 +1,15 @@
 import minimalmodbus
 
 # 모드버스 장치 설정
-instrument = minimalmodbus.Instrument('COM8', 1)  # COM 포트와 장치 주소 설정
+instrument = minimalmodbus.Instrument('COM7', 1)  # COM 포트와 장치 주소 설정
 instrument.serial.baudrate = 9600
 instrument.serial.timeout = 3
 instrument.serial.stopbits = 1
 instrument.serial.bytesize = 8
 
-# LED 제어
-def control_led(color):
+for i in range(0,2):
     try:
-        if color == 0:
-            # 붉은색 LED 제어
-            instrument.write_bit(0, True)
-            # instrument.write_bit(1, False)
-            print("붉은색 LED를 켰습니다.")
-        elif color == 1:
-            # 초록색 LED 제어
-            # instrument.write_bit(0, False)
-            instrument.write_bit(1, True)
-            print("초록색 LED를 켰습니다.")
+        instrument.write_bit(1, True) #RED:0 / GREEN:1
+        print('색상 설정 완료')
     except:
-        print("모드버스 통신 오류")
-
-# 사용자 입력 받기
-while True:
-        choice = int(input("LED 색상을 선택하세요 (0: 붉은색, 1: 초록색): "))
-        if choice == 0 or choice == 1:
-            control_led(choice)
-            break
-        else:
-            print("잘못된 입력입니다. 0 또는 1을 입력하세요.")
+        continue
--- write.py
+++ write.py
@@ -1,14 +1,14 @@
 import minimalmodbus
 
 # 시리얼 포트 설정
-instrument = minimalmodbus.Instrument('COM8', 1)
+instrument = minimalmodbus.Instrument('COM7', 1)
 instrument.serial.baudrate = 9600
 instrument.serial.timeout = 1
 instrument.serial.stopbits = 1
 instrument.serial.bytesize = 8
 
 # 모드버스에 입력되는 숫자
-digital_segment_value = 9999
+digital_segment_value = 7777
 
 for i in range(0, 3):
     try:
