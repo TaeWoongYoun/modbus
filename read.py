cf5ff2f2783b18617d04458a8caed24ac00a4596
xodnd0507 <24457545yong@gmail.com>
2024-07-09 17:11:03 +0900

    test : Update

--- read.py
+++ read.py
@@ -1,7 +1,7 @@
 import minimalmodbus
 
 # 시리얼 포트 설정
-instrument = minimalmodbus.Instrument('COM8', 1)
+instrument = minimalmodbus.Instrument('COM7', 1)
 instrument.serial.baudrate = 9600
 instrument.serial.timeout = 1
 instrument.serial.stopbits = 1
