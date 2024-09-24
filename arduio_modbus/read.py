64f5cc6debc212f04cab9ef3f8a2b00581e7fc00
xodnd0507 <24457545yong@gmail.com>
2024-09-24 09:51:57 +0900

    test : Create function input comport

--- arduio_modbus/read.py
+++ arduio_modbus/read.py
@@ -1,8 +1,9 @@
 import minimalmodbus
 import time
 
-# 시리얼 포트 설정
-instrument = minimalmodbus.Instrument('COM3', 1)
+port = input("사용할 시리얼 포트를 입력하세요 (예: COM1): ")
+
+instrument = minimalmodbus.Instrument(port, 1)
 instrument.serial.baudrate = 9600
 instrument.serial.timeout = 1
 instrument.serial.stopbits = 1
@@ -10,7 +11,10 @@ instrument.serial.bytesize = 8
 
 time.sleep(3)
 
-response = instrument.read_registers(0, 3)
-print(f"거리: {response[0]}")
-print(f"습도: {response[1]}")
-print(f"온도: {response[2]}")
+try:
+    response = instrument.read_registers(0, 3)
+    print(f"거리: {response[0]}")
+    print(f"습도: {response[1]}")
+    print(f"온도: {response[2]}")
+except Exception as e:
+    print(f"시리얼 포트 에러")
