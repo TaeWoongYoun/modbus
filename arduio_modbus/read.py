a3883e45ebb65da0e65e8241808749d921a05a81
xodnd0507 <24457545yong@gmail.com>
2024-09-24 17:29:48 +0900

    test : Update

--- arduio_modbus/read.py
+++ arduio_modbus/read.py
@@ -11,16 +11,13 @@ instrument.serial.stopbits = 1
 instrument.serial.bytesize = 8
 time.sleep(3)
 
-conn = pymysql.connect(host='localhost', user='root', password='1234', db='Analog_read', charset='utf8')
-cursor = conn.cursor()
-
-try:
-    while True:
-        data = instrument.read_registers(0, 3)
-        print(f"거리: {data[0]} / 습도: {data[1]} / 온도: {data[2]}")
-        sql = "INSERT INTO program (Distance, Humidity, Temperature) VALUES ('"+str(data[0])+"', '"+str(data[1])+"', '"+str(data[2])+"')"
-        cursor.execute(sql)
-        conn.commit()
-        time.sleep(15)
-finally:
+while True:
+    data = instrument.read_registers(0, 3)
+    print(f"거리: {data[0]} / 습도: {data[1]} / 온도: {data[2]}")
+    conn = pymysql.connect(host='localhost', user='root', password='1234', db='Analog_read', charset='utf8')
+    cursor = conn.cursor()
+    sql = "INSERT INTO program (Distance, Humidity, Temperature) VALUES ('"+str(data[0])+"', '"+str(data[1])+"', '"+str(data[2])+"')"
+    cursor.execute(sql)
+    conn.commit()
     conn.close()
+    time.sleep(10) 
