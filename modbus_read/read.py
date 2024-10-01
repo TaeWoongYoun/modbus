74eeb129d9734a05e5d1367d13c5c9c32c55f8b2
xodnd0507 <24457545yong@gmail.com>
2024-10-01 14:11:07 +0900

    docs : File change

--- modbus_read/read.py
+++ modbus_read/read.py
@@ -1,18 +1,38 @@
 import minimalmodbus
+import time
+import pymysql
+import threading
 
-# 시리얼 포트 설정
-instrument = minimalmodbus.Instrument('COM7', 1)
+port = input("포트를 입력하세요 (예: COM1): ")
+
+instrument = minimalmodbus.Instrument(port, 1)
 instrument.serial.baudrate = 9600
 instrument.serial.timeout = 1
 instrument.serial.stopbits = 1
 instrument.serial.bytesize = 8
+time.sleep(3)
 
-for i in range(0, 3):
-    try:
-        # 모드버스 RTU 프레임 전송 및 데이터 읽기
-        response = instrument.read_registers(0, 3)
-        print(f"거리: {response[0]}")
-        print(f"습도: {response[1]}")
-        print(f"온도: {response[2]}")
-    except:
-        continue  # 예외 발생 시 다시 시도
+# 종료 플래그
+running = True
+def check_exit():
+    global running
+    while True:
+        user_input = input()
+        if user_input.lower() == '종료':
+            running = False
+            print("프로그램을 종료하는 중 입니다. 잠시만 기다려주세요.")
+            break
+threading.Thread(target=check_exit, daemon=True).start()
+
+try: 
+    while running:
+        data = instrument.read_registers(0, 3)
+        print(f"거리: {data[0]} / 습도: {data[1]} / 온도: {data[2]}")
+        conn = pymysql.connect(host='localhost', user='root', password='1234', db='Analog_read', charset='utf8')
+        cursor = conn.cursor()
+        sql = "INSERT INTO program (Distance, Humidity, Temperature) VALUES ('"+str(data[0])+"', '"+str(data[1])+"', '"+str(data[2])+"')"
+        conn.commit()
+        conn.close()
+        time.sleep(10)
+finally:
+    print("프로그램이 정상적으로 종료되었습니다.")
