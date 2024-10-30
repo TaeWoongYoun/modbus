cbce151d9d4f8d10acfaed97b8e074b2d4599dea
xodnd0507 <24457545yong@gmail.com>
2024-10-31 00:47:35 +0900

    docs : Change file name

---
+++ arduio_modbus/test.py
@@ -0,0 +1,22 @@
+import Analog_read
+import Analog_write
+import BIT_LED
+import time
+time.sleep(2)
+
+while True:
+    number = input("숫자 1(read), 2(write), 3(LED) 중 하나를 입력하세요: ")
+    
+    if number.isdigit():
+        number = int(number)
+        if number == 1:
+            print("read모드를 선택하셨습니다.")
+            Analog_read.read_register()
+        elif number == 2:
+            print("write모드를 선택하셨습니다.")
+            Analog_write.write_register()
+        elif number == 3:
+            print("LED모드를 선택하셨습니다.")
+            BIT_LED.write_bit()
+    else:
+        print("숫자를 입력하세요.")
